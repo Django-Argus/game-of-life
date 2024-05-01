1218cf79a7f3b53cbf9ededdad858eead05a7842
Django-Argus <malaska@mailo.com>
2024-05-01 15:32:56 +0200

    Add fullscreen mode

--- src/net/argus/game/gol/GameFrame.java
+++ src/net/argus/game/gol/GameFrame.java
@@ -1,5 +1,8 @@
 package net.argus.game.gol;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.JFrame;
 
 public class GameFrame extends JFrame {
@@ -8,6 +11,8 @@ public class GameFrame extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 2340582052636536008L;
+	
+	private boolean fullScreen = false;
 	
 	public GameFrame(GamePanel mainComp) {
 		super("Game Of Life");
@@ -19,6 +24,30 @@ public class GameFrame extends JFrame {
 		setLocationRelativeTo(null);
 		
 		setDefaultCloseOperation(3);
+		JFrame thiss = this;
+		
+		addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if(e.getKeyCode() == KeyEvent.VK_F11) {
+					if(!fullScreen) {
+						getGraphicsConfiguration().getDevice().setFullScreenWindow(thiss);
+						fullScreen = true;
+					}else {
+						getGraphicsConfiguration().getDevice().setFullScreenWindow(null);
+						fullScreen = false;
+					}
+				}
+				
+			}
+		});
 	}
 	
 }
